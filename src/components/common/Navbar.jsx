76ee9eb22953fc60f6ddb9872bcd84b26b11db2c
manikantjha <being.manikantjha@gmail.com>
2022-07-16 21:23:36 +0530

    favicon and name changed

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
           sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}
         >
           <img
-            src="/bloom-media-react-app/bloom-media-logo-white.svg"
+            src="/bloom-media-react-app/logo.svg"
             alt="logo"
             style={{ width: "100px", height: "auto" }}
           />
