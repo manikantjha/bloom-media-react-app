eb43a53b5628ac3a690c5394fd1ec874cfe9a25e
manikantjha <being.manikantjha@gmail.com>
2022-07-27 22:52:30 +0530

    coming spelling fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div>
       <Hero
         heroImg="https://images.unsplash.com/photo-1657672733176-b48c9b0eec0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
-        heroTitle="Comming Soon"
+        heroTitle="Coming Soon"
         heroDescription="The website is moving towards its final developments and will be live shortly. Can't wait to see you too! Till then..."
         btnText="Breath In... Breathe Out..."
         noBtn
